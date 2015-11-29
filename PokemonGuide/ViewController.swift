e458176ae5d1727522dde962561f283fbd81ddce
Denis Motta <denismotta@deniss-mbp.home>
2015-11-29 15:32:52 -0200

    Initial Commit

---
+++ PokemonGuide/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PokemonGuide
+//
+//  Created by Denis Motta on 11/29/15.
+//  Copyright © 2015 Denis Motta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
